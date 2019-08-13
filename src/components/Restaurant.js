26a31ecdb2be4d530f9cb871df60eeb7c23cdb5e
Olga Fomin <olga.fomin26@gmail.com>
2019-08-12 20:40:57 -0700

    Add CardActions to restaurant card

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography, Divider, Box } from "@material-ui/core";
+import { Card,
+        CardContent,
+        CardActions,
+        CardMedia,
+        Typography,
+        Divider,
+        Box,
+        IconButton,
+        Button } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     card: {
       maxWidth: 300,
-      margin: "auto",
-      transition: "0.3s",
-      boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-      "&:hover": {
-        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+      margin: 'auto',
+      transition: '0.3s',
+      boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
+      '&:hover': {
+        boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)'
       }
     },
     media: {
-      paddingTop: "56.25%"
+      paddingTop: '56.25%'
     },
     content: {
-      textAlign: "left",
-      padding: theme.spacing(3)
+      textAlign: 'left',
+      padding: theme.spacing(3),
+      paddingBottom: '0'
     },
     rating: {
         width: 200,
@@ -26,7 +36,7 @@ const useStyles = makeStyles(theme => ({
         alignItems: 'center',
     },
     divider: {
-      margin: `${theme.spacing(3)}px 0`
+      margin: `${theme.spacing(3)}px 0 ${theme.spacing(1)}px 0`
     }
 }));
 
@@ -41,36 +51,46 @@ function Restaurant(props) {
     return (
         <Card className={classes.card}>
             <CardMedia
+                alt={`Thumbnail of ${name}`}
                 className={classes.media}
                 image={ thumb } />
             <CardContent className={classes.content}>
-            <Typography
-                variant={"h5"}
-                gutterBottom>
-                { name }
-            </Typography>
-            <Box component="fieldset" mb={2} borderColor="transparent">
-                <div className={classes.rating}>
-                    <Rating value={rating} size="small" precision={0.1} readOnly />
-                    <Box ml={1}>
-                        <Typography variant="caption">
-                            {rating} ({votes})
-                        </Typography>
-                    </Box>
-                </div>
-            </Box>
-            <Typography 
-                variant="subtitle1" 
-                gutterBottom>
-                { locality }
-            </Typography>
-            <Typography 
-                variant={"caption"}>
-                { address }
-            </Typography>
-            <Divider className={classes.divider} light />
-            <button>View Restaurant Details</button>
+                <Typography
+                    component='h2'
+                    variant={'h5'}
+                    gutterBottom>
+                    { name }
+                </Typography>
+                <Box component='fieldset' mb={2} borderColor='transparent'>
+                    <div className={classes.rating}>
+                        <Rating value={rating} size='small' precision={0.1} readOnly />
+                        <Box ml={1}>
+                            <Typography variant='caption'>
+                                {rating} ({votes})
+                            </Typography>
+                        </Box>
+                    </div>
+                </Box>
+                <Typography 
+                    variant='subtitle1' 
+                    gutterBottom>
+                    { locality }
+                </Typography>
+                <Typography 
+                    variant='body2' color='textSecondary' component='p'>
+                    { address }
+                </Typography>
+                <Divider className={classes.divider} light />
             </CardContent>
+
+            <CardActions disableSpacing>
+                <IconButton aria-label='add to favorites'>
+                    <FavoriteIcon />
+                </IconButton>
+                <Button size='small' color='primary'>
+                    View Restaurant Details
+                </Button>
+            </CardActions>
         </Card>
     );
 }
